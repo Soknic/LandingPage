4c55988249016dac4ba3f66bc984a1073f876586
stefan <soknics@gmail.com>
2021-06-10 04:26:23 +0200

    Peglanje i scroll reveal

--- assets/js.js
+++ assets/js.js
@@ -20,28 +20,20 @@ function linkAction() {
 }
 navLink.forEach((n) => n.addEventListener("click", linkAction));
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
-const sections = document.querySelectorAll("section[id]");
+const links = document.querySelectorAll('.nav__link');
+const sections = document.querySelectorAll('section');
 
-function scrollActive() {
-  const scrollY = window.pageYOffset;
+function changeLinkState() {
+  let index = sections.length;
 
-  sections.forEach((current) => {
-    const sectionHeight = current.offsetHeight;
-    const sectionTop = current.offsetTop - 50;
-    sectionId = current.getAttribute("id");
-
-    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-      document
-        .querySelector(".nav__menu a[href*=" + sectionId + "]")
-        .classList.add("active-link");
-    } else {
-      document
-        .querySelector(".nav__menu a[href*=" + sectionId + "]")
-        .classList.remove("active-link");
-    }
-  });
+  while(--index && window.scrollY + 20 < sections[index].offsetTop) {}
+  
+  links.forEach((link) => link.classList.remove('active-link'));
+  links[index].classList.add('active-link');
 }
-window.addEventListener("scroll", scrollActive);
+
+changeLinkState();
+window.addEventListener('scroll', changeLinkState);
 /*==================== CHANGE BACKGROUND HEADER ====================*/
 function scrollHeader() {
   const nav = document.getElementById("header");
@@ -60,27 +52,50 @@ function scrollTop() {
 window.addEventListener("scroll", scrollTop);
 
 /*==================== DARK LIGHT THEME ====================*/
-const themeButton=document.getElementById("theme-button")
-const darkTheme="dark-theme"
-const iconTheme="bx-sun"
+const themeButton = document.getElementById("theme-button");
+const darkTheme = "dark-theme";
+const iconTheme = "bx-sun";
 
-const selectedTheme=localStorage.getItem("selected-theme")
-const selectedIcon=localStorage.getItem("selected-icon")
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
 
-const getCurrentTheme=()=>document.body.classList.contains(darkTheme)? "dark":"light"
-const getCurrentIcon=()=>themeButton.classList.contains(iconTheme)? "bx-moon":"bx-sun"
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? "dark" : "light";
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme) ? "bx-moon" : "bx-sun";
 
-if(selectedTheme){
-  document.body.classList[selectedTheme=== "dark"?"add":"remove"](darkTheme)
-  themeButton.classList[selectedIcon=== "bx-moon"?"add":"remove"](iconTheme)
-
+if (selectedTheme) {
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+    darkTheme
+  );
+  themeButton.classList[selectedIcon === "bx-moon" ? "add" : "remove"](
+    iconTheme
+  );
 }
 
-themeButton.addEventListener("click",()=>{
-  document.body.classList.toggle(darkTheme)
-  themeButton.classList.toggle(iconTheme)
+themeButton.addEventListener("click", () => {
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
 
-  localStorage.setItem("selected-theme",getCurrentTheme())
-  localStorage.setItem("selected-icon",getCurrentIcon())
+  localStorage.setItem("selected-theme", getCurrentTheme());
+  localStorage.setItem("selected-icon", getCurrentIcon());
+});
+/*==================== SCROLL REVEAL ANIMATION ====================*/
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "30px",
+  duration: 2000,
+  reset: true,
+});
 
-})
+sr.reveal(
+  `.home__data, .home__img,
+          .about__data, .about__img,
+          .services__content, .menu__content,
+          .app__data, .app__img,
+          .contact__data, .contact__button,
+          .footer__content`,
+  {
+    interval: 200,
+  }
+);
